055177046800dd15d5abeeb1f78bd9b3173839ac
Kylan Michael Johnson <103906447+kylanj7@users.noreply.github.com>
2025-11-30 11:53:37 -0800

    Create VenvSetup.sh

---
+++ VenvSetup.sh
@@ -0,0 +1,24 @@
+# Automatic setup of Python Virtual Environment
+
+read -p "Enter Envoronment Name: " VENV_DIR
+
+sudo apt update
+sudo apt install python3.10-env
+
+# ---Make Python env & install requirements---
+echo "Creating Python virtual environment..."
+if [ ! -d "$VENV_DIR" ]; then
+    python3 -m venv "$VENV_DIR"
+else
+    echo "Virtual environment '$VENV_DIR' already exists."
+fi
+
+echo "Activating virtual environment and installing requirements..."
+source "$VENV_DIR/bin/activate"
+
+if [ -f "requirements.txt" ]; then
+    pip install -r requirements.txt
+    echo "Python requirements installed."
+else
+    echo "Warning: requirements.txt file not found."
+fi
