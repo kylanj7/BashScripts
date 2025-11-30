0af1e9423d981aa40c8bfb8189f90e7842b51d8f
Kylan Michael Johnson <103906447+kylanj7@users.noreply.github.com>
2025-11-30 12:12:20 -0800

    Create RemoveOllama.sh

---
+++ RemoveOllama.sh
@@ -0,0 +1,9 @@
+# Remove Ollama Installtion from Linux System
+sudo systemctl stop ollama
+sudo systemctl disable ollama
+sudo rm /etc/systemd/system/ollama.service
+sudo systemctl daemon-reload
+sudo rm -rf /usr/local/bin/ollama /usr/local/lib/ollama
+sudo rm -rf /usr/share/ollama ~/.ollama
+sudo userdel ollama
+sudo groupdel ollama
