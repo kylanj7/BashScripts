f38e0290b509d05a40579496e9c67fd9ea035b35
Kylan Michael Johnson <103906447+kylanj7@users.noreply.github.com>
2025-12-14 09:37:28 -0800

    Update RHELsub.sh

--- RHELsub.sh
+++ RHELsub.sh
@@ -10,9 +10,12 @@ sudo subscription-manager  config --server.proxy_hostname=proxy-location.company
 
 read -p "Enter Client Activation Key: " KEY
 read -p  "Enter Server's Domain Name: " SERVER_DNS # Example: servername.location.company.com
+read -p "Enter Proxy: " PROXY # Example: proxy-location.company.com
+read -p "Enter Proxy Port: " PORT
 read -p "Enter Organization Key: " ORG_KEY
 
 #REGISTER
+sudo subscription-manager  config --server.proxy_hostname=$PROXY --server.proxy_port=$PORT
 sudo subscription-manager register $KEY --org $ORG_KEY $SERVER_DNS
 sudo subscription-manager identity
 sudo subscription-manager status
