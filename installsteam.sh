7cc8f52812ea604277bef8681b7cd705221d187d
Kylan Michael Johnson <103906447+kylanj7@users.noreply.github.com>
2025-12-28 23:04:19 -0800

    Create installsteam.sh

---
+++ installsteam.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+sudo dnf update --refresh
+
+sudo dnf install steam -y
+
+read -p "Run Steam? (y/n) " RUN
+
+if [[ "$RUN" == "y" ]]; then
+    
+    steam
+else
+    exit 0
+fi
