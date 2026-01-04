069b0781422a83ffc8fa48edb80b6aa57f3aa749
Kylan Michael Johnson <103906447+kylanj7@users.noreply.github.com>
2026-01-04 11:37:19 -0800

    Update oldreleases.sh

--- oldreleases.sh
+++ oldreleases.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo sed -i -e 's|http://archive.ubuntu.com|http://old-releases.ubuntu.com|g' /etc/apt/sources.list
 sudo sed -i -e 's|http://security.ubuntu.com|http://old-releases.ubuntu.com|g' /etc/apt/sources.list
 sudo apt update
