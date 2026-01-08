42c94931918e7e7446e8f7e349bdffa794c734e1
Kylan Michael Johnson <103906447+kylanj7@users.noreply.github.com>
2026-01-08 06:37:49 -0800

    Create install_vscode.sh

---
+++ install_vscode.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+dnf check-update
+
+sudo dnf install code
+
+read -p "Run VS Code?: (y/n)" RUN
+
+if [[ "$RUN" == "y" ]]; then
+    code
+else
+    exit
+fi
